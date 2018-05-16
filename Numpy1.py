54db5cca9659e3d0f775e6b0fcbc83aae2a976a8
bhavnajindal <bhavna.jindal@yahoo.com>
2018-05-16 22:08:09 +0530

    My basic Numpy exercises

---
+++ Numpy1.py
@@ -0,0 +1,17 @@
+#First Numpy array
+
+# Create list baseball
+
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Import the numpy package as np
+
+import numpy as np 
+
+# Create a Numpy array from baseball: np_baseball
+
+np_baseball = np.array(baseball, int)
+
+# Print out type of np_baseball
+
+print(type(np_baseball))
---
+++ numpyOperations.py
@@ -0,0 +1,20 @@
+#Operations on numpy array
+# Import numpy
+
+import numpy as np
+
+# Create a Numpy array from height: np_height
+
+np_height = np.array(height)
+
+# Print out np_height
+
+print(np_height)
+
+# Convert np_height to m: np_height_m
+
+np_height_m = np_height * 0.0254
+
+# Print np_height_m
+
+print(np_height_m)
